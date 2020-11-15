af06b00e4eb1b6e00958061810338418d34eed0a
YuYue <9987jdvrb@gmail.com>
2020-11-15 15:45:11 +0800

    add normal

--- src/main.cpp
+++ src/main.cpp
@@ -14,76 +14,11 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 using namespace vex;
-double m_quickStopAccumulator = 0, leftout, rightout;
+// double m_quickStopAccumulator = 0, leftout, rightout;
 void armMotorForward(){
   armmotor.spin(forward, 20, pct);
 }
-double max(double val, double num){
-  if(val >= num)
-    return val;
-  else 
-    return num;
-}
-void curvatureDrive(double xSpeed, double zRotation, bool isQuickTurn) {
-    // xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
-    if(xSpeed < 0.05 & xSpeed > -0.05){
-      xSpeed = 0;
-    }//簡易死區設定
-    // zRotation = MathUtil.clamp(zRotation, -1.0, 1.0);
-    double angularPower;
-    bool overPower;
-    double m_quickStopAlpha = 0.1;
-    if (isQuickTurn) {
-      if (sqrt(pow(xSpeed, 2)) < 0.1) {
-        m_quickStopAccumulator = (1 - 0.1) * m_quickStopAccumulator
-            + m_quickStopAlpha * zRotation * 2;
-      }
 
-      overPower = true;
-      angularPower = zRotation;
-    } else {
-      overPower = false;
-      angularPower = sqrt(pow(xSpeed, 2)) * zRotation - m_quickStopAccumulator;
-
-      if (m_quickStopAccumulator > 1) {
-        m_quickStopAccumulator -= 1;
-      } else if (m_quickStopAccumulator < -1) {
-        m_quickStopAccumulator += 1;
-      } else {
-        m_quickStopAccumulator = 0.0;
-      }
-    }
-
-    double leftMotorOutput = xSpeed - angularPower;
-    double rightMotorOutput = xSpeed + angularPower;
-
-    // If rotation is overpowered, reduce both outputs to within acceptable range
-    if (overPower) {
-      if (leftMotorOutput > 1.0) {
-        rightMotorOutput -= leftMotorOutput - 1.0;
-        leftMotorOutput = 1.0;
-      } else if (rightMotorOutput > 1.0) {
-        leftMotorOutput -= rightMotorOutput - 1.0;
-        rightMotorOutput = 1.0;
-      } else if (leftMotorOutput < -1.0) {
-        rightMotorOutput -= leftMotorOutput + 1.0;
-        leftMotorOutput = -1.0;
-      } else if (rightMotorOutput < -1.0) {
-        leftMotorOutput -= rightMotorOutput + 1.0;
-        rightMotorOutput = -1.0;
-      }
-    }
-
-    // Normalize the wheel speeds
-    double maxMagnitude = max(sqrt(pow(leftMotorOutput, 2)), sqrt(pow(rightMotorOutput, 2)));
-    if (maxMagnitude > 1.0) {
-      leftMotorOutput /= maxMagnitude;
-      rightMotorOutput /= maxMagnitude;
-    }
-    leftout=  leftMotorOutput;
-    rightout = rightMotorOutput;
-
-  }
 int main() {
 
   vexcodeInit();
@@ -93,8 +28,8 @@ int main() {
   
   while(1){
     //輪子
-    // int left = Controller1.Axis2.position(percent) + Controller1.Axis1.position(percent) + Controller1.Axis3.position(percent) + Controller1.Axis4.position(percent);
-    // int right = Controller1.Axis2.position(percent) - Controller1.Axis1.position(percent) + Controller1.Axis3.position(percent) - Controller1.Axis4.position(percent);
+    int left = Controller1.Axis2.position(percent) + Controller1.Axis1.position(percent) + Controller1.Axis3.position(percent) + Controller1.Axis4.position(percent);
+    int right = Controller1.Axis2.position(percent) - Controller1.Axis1.position(percent) + Controller1.Axis3.position(percent) - Controller1.Axis4.position(percent);
 
     // for(int i = 0; i < 1000; i++){
     //   left = Controller1.Axis2.position(percent) + Controller1.Axis1.position(percent) + Controller1.Axis3.position(percent) + Controller1.Axis4.position(percent);
@@ -103,9 +38,9 @@ int main() {
     //   motionL[i] = left;
     //   motionR[i] = right;
     // }
-    curvatureDrive(Controller1.Axis2.position(percent), -Controller1.Axis4.position(percent), Controller1.ButtonR1.pressing() || Controller1.ButtonL1.pressing());
-    leftmotor.spin(forward, leftout * 30, pct);
-    rightmotor.spin(forward, rightout * 30, pct);
+    // curvatureDrive(Controller1.Axis2.position(percent), -Controller1.Axis4.position(percent), Controller1.ButtonR1.pressing() || Controller1.ButtonL1.pressing());
+    leftmotor.spin(forward, left * 50, pct);
+    rightmotor.spin(forward, right * 50, pct);
     // vex::task::sleep(100);
 
     // for(int i = 0; i < 1000; i++){
